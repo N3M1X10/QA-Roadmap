48857b35819da233dfaad413f4bcc6f89a94aa9a
N3M1X10 <48549372+N3M1X10@users.noreply.github.com>
2026-07-16 00:33:14 +0500

    Update fisrt.silly.test.js
    added test for empty-lines login button click (additional example for repeating)

--- 4_Playwright_Auto/sandbox/tests/fisrt.silly.test.js
+++ 4_Playwright_Auto/sandbox/tests/fisrt.silly.test.js
@@ -29,3 +29,21 @@ test('Ошибка авторизации при неверном пароле',
     await expect(errorMessage).toBeVisible();
     await expect(errorMessage).toContainText('Epic sadface: Username and password do not match');
 });
+
+test('Отображение ошибки при попытке входа с пустыми полями', async ({ page }) => {
+    // 1. Arrange: Открываем сайт
+    await page.goto('https://www.saucedemo.com/');
+
+    // 2. Act: Твоё действие. Нам нужно просто кликнуть по кнопке войти, ничего не вводя.
+    await page.locator('#login-button').click();
+
+
+    // 3. Assert: Проверяем, что появилась плашка с ошибкой.
+    // Локатор для текста ошибки на этом сайте можно найти по специальному атрибуту: page.locator('[data-test="error"]')
+
+    const errorMessage = page.locator('[data-test="error"]');
+
+    await expect(errorMessage).toBeVisible();
+    await expect(errorMessage).toContainText('Epic sadface: Username is required');
+
+});
