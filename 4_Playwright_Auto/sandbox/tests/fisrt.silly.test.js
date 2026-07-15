044736b9fbc935015b51cb4c542c6d37e9ba1968
N3M1X10 <48549372+N3M1X10@users.noreply.github.com>
2026-07-16 00:42:49 +0500

    Update fisrt.silly.test.js
    added const for saucedemo site

--- 4_Playwright_Auto/sandbox/tests/fisrt.silly.test.js
+++ 4_Playwright_Auto/sandbox/tests/fisrt.silly.test.js
@@ -1,9 +1,11 @@
 // @ts-check
 const { test, expect } = require('@playwright/test');
 
+const sauceSite = 'https://www.saucedemo.com/';
+
 test('Успешная авторизация с валидными учетными данными', async ({ page }) => {
     // 1. Arrange: Открываем страницу авторизации
-    await page.goto('https://www.saucedemo.com/');
+    await page.goto(sauceSite);
 
     // 2. Act: Вводим логин, пароль и нажимаем кнопку входа
     await page.locator('#user-name').fill('standard_user');
@@ -17,7 +19,7 @@ test('Успешная авторизация с валидными учетны
 
 test('Ошибка авторизации при неверном пароле', async ({ page }) => {
     // 1. Arrange
-    await page.goto('https://www.saucedemo.com/');
+    await page.goto(sauceSite);
 
     // 2. Act: Вводим правильный логин, но неверный пароль
     await page.locator('#user-name').fill('standard_user');
@@ -32,7 +34,7 @@ test('Ошибка авторизации при неверном пароле',
 
 test('Отображение ошибки при попытке входа с пустыми полями', async ({ page }) => {
     // 1. Arrange: Открываем сайт
-    await page.goto('https://www.saucedemo.com/');
+    await page.goto(sauceSite);
 
     // 2. Act: Твоё действие. Нам нужно просто кликнуть по кнопке войти, ничего не вводя.
     await page.locator('#login-button').click();
